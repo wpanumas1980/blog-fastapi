bbe3c815e821eec98d08532abfa82685742e291f
Panumas Saewong <wpanumas@gmail.com>
2022-09-13 07:55:54 +0700

    create blogs repository

---
+++ repository/blogs.py
@@ -0,0 +1,41 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+from schemas import Blogs, UpdateBlog, ShowBlogs
+from database import get_db
+import models
+
+def create(request: Blogs, db: Session):
+    new_blog = models.Blogs(title=request.title,description=request.description,user_id=1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+def find( db: Session):
+    blogs = db.query(models.Blogs).all()
+    return blogs
+
+def findOne(id, db: Session):
+    blog = db.query(models.Blogs).filter(models.Blogs.id == id).first()
+    if not blog:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
+    return blog
+
+def update(id,request, db: Session):
+    blog = db.query(models.Blogs).filter(models.Blogs.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
+    update_item_encoded = jsonable_encoder(request.dict(exclude_unset=True))
+    blog.update(update_item_encoded)
+    db.commit()
+    return 'updated'
+
+def destroy(id:int,db: Session):
+          blog = db.query(models.Blogs).filter(models.Blogs.id == id)
+          if not blog.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
+          blog.delete(synchronize_session=False)
+          db.commit()
+          return 'done'
--- routers/blogs.py
+++ routers/blogs.py
@@ -6,47 +6,29 @@ from schemas import Blogs, UpdateBlog, ShowBlogs
 from database import get_db
 import models
 
+from repository import blogs
+
 router = APIRouter(
     prefix='/blogs',
     tags=['Blogs']
 )
 
-@router.get('/',response_model=List[ShowBlogs])
-def find( db: Session = Depends(get_db)):
-    blogs = db.query(models.Blogs).all()
-    return blogs
-
-
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: Blogs, db: Session = Depends(get_db)):
-    new_blog = models.Blogs(title=request.title,description=request.description,user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blogs.create(request,db)
+
+@router.get('/',response_model=List[ShowBlogs])
+def find( db: Session = Depends(get_db)):
+    return blogs.find(db)
 
 @router.get('/{id}', status_code=200,response_model=ShowBlogs)
 def findOne(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blogs).filter(models.Blogs.id == id).first()
-    if not blog:
-          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
-    return blog
-
-@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id,db: Session = Depends(get_db)):
-          blog = db.query(models.Blogs).filter(models.Blogs.id == id)
-          if not blog.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
-          blog.delete(synchronize_session=False)
-          db.commit()
-          return 'done'
+    return blogs.findOne(id,db)
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id,request: UpdateBlog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blogs).filter(models.Blogs.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog {id} is not found!')
-    update_item_encoded = jsonable_encoder(request.dict(exclude_unset=True))
-    blog.update(update_item_encoded)
-    db.commit()
-    return 'updated'
+    return blogs.update(id,request,db)
+
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db: Session = Depends(get_db)):
+          return blogs.destroy(id,db)
