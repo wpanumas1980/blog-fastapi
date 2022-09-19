975ab7b6a20e37d029e19cfc23d8b34b17f493cf
Panumas Saewong <wpanumas@gmail.com>
2022-09-19 17:29:33 +0700

    create token

--- routers/authenticate.py
+++ routers/authenticate.py
@@ -3,6 +3,7 @@ from schemas import Login
 from sqlalchemy.orm import Session
 from database import get_db
 from hashing import Hash
+from jwt import create_access_token
 import models
 
 router = APIRouter(
@@ -11,9 +12,14 @@ router = APIRouter(
 
 @router.post('/login')
 def login(request:Login, db: Session = Depends(get_db)):
-    user = db.query(models.Users).filter(models.Users.email == request.username).first()
+    user = db.query(models.Users).filter(models.Users.email == request.email).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Invalid Credentials')
-    if Hash.verify(request.password , user.password):
+    if not Hash.verify(request.password , user.password):
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Incorrect password')
-    return user
+
+    # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
--- schemas.py
+++ schemas.py
@@ -34,5 +34,10 @@ class ShowBlogs(BaseModel):
         orm_mode = True
 
 class Login(BaseModel):
-    username:str
+    email:str
     password:str
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+class TokenData(BaseModel):
+    email: str
