7179adfa919d746d5a42c86948dcbc8e957c3343
Panumas Saewong <wpanumas@gmail.com>
2022-09-19 08:07:17 +0700

    add .env and config

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@192.168.1.100/fast_blogs"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@192.168.1.100/fast_blogs"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- hashing.py
+++ hashing.py
@@ -4,5 +4,4 @@ pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hash():
     def bcrypt(password:str):
-        
         return pwd_ctx.hash(password)
