cff0b27e9665f6a601c8d6d8c4f1567507997cf6
Panumas Saewong <wpanumas@gmail.com>
2022-09-23 14:56:07 +0700

    verify token

--- routers/authenticate.py
+++ routers/authenticate.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from schemas import Login
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from database import get_db
 from hashing import Hash
@@ -11,8 +11,9 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request:Login, db: Session = Depends(get_db)):
-    user = db.query(models.Users).filter(models.Users.email == request.email).first()
+def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    print('request =>', request)
+    user = db.query(models.Users).filter(models.Users.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Invalid Credentials')
     if not Hash.verify(request.password , user.password):
--- routers/blogs.py
+++ routers/blogs.py
@@ -1,9 +1,9 @@
 from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from schemas import Blogs, UpdateBlog, ShowBlogs
+from schemas import Blogs, UpdateBlog, ShowBlogs, Users
 from database import get_db
-
+from oauth2 import get_current_user
 from repository import blogs
 
 router = APIRouter(
@@ -12,21 +12,22 @@ router = APIRouter(
 )
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: Blogs, db: Session = Depends(get_db)):
+def create(request: Blogs, db: Session = Depends(get_db),current_user:Users = Depends(get_current_user)):
     return blogs.create(request,db)
 
 @router.get('/',response_model=List[ShowBlogs])
-def find( db: Session = Depends(get_db)):
+def find( db: Session = Depends(get_db), current_user:Users = Depends(get_current_user)):
+# def find( db: Session = Depends(get_db)):
     return blogs.find(db)
 
-@router.get('/{id}', status_code=200,response_model=ShowBlogs)
-def findOne(id, db: Session = Depends(get_db)):
+@router.get('/{id}', status_code=200,response_model=ShowBlogs,)
+def findOne(id, db: Session = Depends(get_db),current_user:Users = Depends(get_current_user)):
     return blogs.findOne(id,db)
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id,request: UpdateBlog, db: Session = Depends(get_db)):
+def update(id,request: UpdateBlog, db: Session = Depends(get_db),current_user:Users = Depends(get_current_user)):
     return blogs.update(id,request,db)
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id,db: Session = Depends(get_db)):
+def destroy(id,db: Session = Depends(get_db),current_user:Users = Depends(get_current_user)):
           return blogs.destroy(id,db)
